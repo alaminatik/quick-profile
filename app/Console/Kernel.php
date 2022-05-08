90b5fd50b84f9c64f430cdf53e2103d0287e1ae8
alamin555 <alaminatekur111@gmail.com>
2022-05-08 14:40:57 +0600

    Last Update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('rating:increase')->everyMinute();
+        $schedule->command('rating:increase')->daily();
     }
 
     /**
